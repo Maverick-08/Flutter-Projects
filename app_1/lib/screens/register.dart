647d1029a0f639e583225f5ea0e2cf32d692e8c2
Vivek <ojhavivek24@gmail.com>
2024-08-17 03:22:44 +0530

    added register screen logic

--- app_1/lib/screens/register.dart
+++ app_1/lib/screens/register.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:app_1/widgets/customButton.dart';
 
 class Register extends StatelessWidget {
-  const Register({super.key});
+  const Register({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: 0.5 * screenHeight,
+        margin: const EdgeInsets.fromLTRB(0, 24, 0, 0),
+        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Register',
+                style: TextStyle(fontSize: 36, color: Colors.black),
+              ),
+            ),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: "Enter your email",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 16), // Use height for vertical spacing
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: "Enter your password",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 30), // Use height for vertical spacing
+                  CustomButton(
+                    text: 'Register',
+                    textColor: Colors.white,
+                    paddingLeft: 0,
+                    paddingTop: 4,
+                    paddingRight: 0,
+                    paddingBottom: 4,
+                    backgroundColor: Colors.black,
+                    onPressed: () {
+                      // Handle log in action
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
