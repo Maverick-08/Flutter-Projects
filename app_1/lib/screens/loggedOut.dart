f7d4265f290f2ac9e316d89034746a87260c38d7
Vivek <ojhavivek24@gmail.com>
2024-08-17 03:13:50 +0530

    added screens templates

---
+++ app_1/lib/screens/loggedOut.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Loggedout extends StatelessWidget {
+  const Loggedout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
