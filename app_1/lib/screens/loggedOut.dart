0f01f588592ce5ade106a4706c5d00244dfc36c6
Vivek <ojhavivek24@gmail.com>
2024-08-17 03:19:44 +0530

    added logged out screen logic

--- app_1/lib/screens/loggedOut.dart
+++ app_1/lib/screens/loggedOut.dart
@@ -5,6 +5,56 @@ class LoggedOut extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        height: screenHeight,
+        width: screenWidth,
+        child: Column(
+          children: [
+            Container(
+                width: screenWidth,
+                height: 0.85 * screenHeight,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/background.jpg'),
+                      fit: BoxFit.cover),
+                )),
+            Container(
+              width: screenWidth,
+              margin: EdgeInsets.fromLTRB(0, 16, 0, 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CustomButton(
+                      text: 'Log In',
+                      textColor: Colors.black,
+                      backgroundColor: Colors.white,
+                      paddingLeft: 16,
+                      paddingRight: 16,
+                      paddingTop: 2,
+                      paddingBottom: 2,
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      }),
+                  CustomButton(
+                      text: 'Register',
+                      textColor: Colors.white,
+                      backgroundColor: Colors.black,
+                      paddingLeft: 16,
+                      paddingRight: 16,
+                      paddingTop: 2,
+                      paddingBottom: 2,
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/register');
+                      })
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
   }
 }
