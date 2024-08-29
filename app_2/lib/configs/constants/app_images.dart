67f6fc248119998f5c9592ed8d2ffb40b745042d
Vivek <ojhavivek24@gmail.com>
2024-08-29 12:14:45 +0530

    added static imports

---
+++ app_2/lib/configs/constants/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const String burn = "assets/icons/burn.png";
+  static const String distance = "assets/icons/distance.png";
+  static const String sleep = "assets/icons/sleep.png";
+  static const String steps = "assets/icons/steps.png";
+  static const String avatar = "assets/icons/avatar.png";
+}
---
+++ app_2/lib/configs/constants/app_vectors.dart
@@ -0,0 +1,6 @@
+class AppVectors {
+  static const String documents = "assets/icons/Documents.svg";
+  static const String folder = "assets/icons/folder.svg";
+  static const String media = "assets/icons/media.svg";
+  static const String unknown = "assets/icons/unknown.svg";
+}
